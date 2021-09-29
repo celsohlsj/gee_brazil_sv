2d7ce370a31c4ab5da91e4428994e9eed5cab7d0
Celso H. L. Silva Junior <celsohlsj@gmail.com>
2021-09-29 11:46:48 -0300

    v4.0.0

--- gee_brazil_sv_code.js
+++ gee_brazil_sv_code.js
@@ -1,46 +1,42 @@
-// Brazilian Secondary Forests Maping
-// ***********************************************************************************************************************************************************************
-//  * Institution:    National institute for space Research (INPE)
-//  * Purpose:        Map the increment, extent, age and loss of secondary forests in Brazil.
-//  * Author:         Celso H. L. Silva Junior
-//  * Email:          celsohlsj@gmail.com
-//  * Please cite as: Silva Junior, C.H.L. et al. Benchmark maps of 33 years of secondary forest age for Brazil. Scientific Data (2020). doi: 10.1038/s41597-020-00600-4
-// ***********************************************************************************************************************************************************************
+// Brazilian Secondary Forests Maping v4
+// ******************************************************************************************
+//  * Institution:  National institute for space Research (INPE)
+//  * Purpose:      Map the increment, extent, age and loss of secondary forests in Brazil.
+//  * Author:       Celso H. L. Silva Junior
+//  * Email:        celsohlsj@gmail.com
+// ******************************************************************************************
 
-// 0. MapBiomas Data (Colection 4.1)
+// 0. MapBiomas Data (Colection 6.0)
 var brazil = ee.FeatureCollection("users/celsohlsj/brazil");
-var mapbiomas = ee.Image('projects/mapbiomas-workspace/public/collection4_1/mapbiomas_collection41_integration_v1').byte();
+var mapbiomas = ee.Image('projects/mapbiomas-workspace/public/collection6/mapbiomas_collection60_integration_v1').byte(); //MapBiomas Collection 5
+//https://mapbiomas-br-site.s3.amazonaws.com/downloads/Colecction%206/Cod_Class_legenda_Col6_MapBiomas_BR.pdf
 
 // 1. Reclassifying MapBiomas Data #Step 1
 var empty = ee.Image().byte();
 
-for (var i=1; i<35; i++)  {
+for (var i=1; i<37; i++)  {
     var y = 1984+i;
     var year = 'classification_'+y;
-    var oldvalues = ee.List([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]);
-    var newvalues = ee.List([0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
-    var forest = mapbiomas.select(year).remap(oldvalues,newvalues).rename(ee.String(year));
+    var forest = mapbiomas.select(year).eq(3);
     empty = empty.addBands(forest);
 }
 var mapbiomas_forest = empty.select(empty.bandNames().slice(1));
 
 // 1.1 Anthropic and Water Mask
 var empty = ee.Image().byte();
-for (var i=1; i<35; i++)  {
+for (var i=1; i<37; i++)  {
     var y = 1984+i;
     var year = 'classification_'+y;
-    var oldvalues = ee.List([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]);
-    var newvalues = ee.List([0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,1,1,1,0,0,1,0,0,0,0,0,1,0,0,0]);
-    var forest = mapbiomas.select(year).remap(oldvalues,newvalues).rename(ee.String(year));
+    var forest = mapbiomas.select(year).remap([14,15,18,19,39,20,40,41,36,46,47,48,9,21],[1,1,1,1,1,1,1,1,1,1,1,1,1,1]).rename(ee.String(year)).unmask(0);
     empty = empty.addBands(forest);
 }
 var anthropic_mask = empty.select(empty.bandNames().slice(1));
 
-var w_mask = ee.Image("JRC/GSW1_1/GlobalSurfaceWater").select("max_extent").clip(brazil).remap([0,1],[1,0]);
+var w_mask = ee.Image("JRC/GSW1_3/GlobalSurfaceWater").select("max_extent").clip(brazil).remap([0,1],[1,0]);
 
 // 2. Mapping the Annual Increment of Secondary Forests  #Step 2
 var empty = ee.Image().byte();
-for (var i=0; i<33; i++)  {
+for (var i=0; i<35; i++)  {
     var y1 = 1985+i;
     var y2 = 1986+i;
     var year1 = 'classification_'+y1;
@@ -59,15 +55,15 @@ var empty = ee.Image().byte();
 var ext = sforest_all.select('classification_1986');
 ext = ext.rename(ee.String('classification_1986'));
 empty = empty.addBands(ext);
-for (var i=1; i<33; i++)  {
+for (var i=1; i<35; i++)  {
     var y = 1986+i;
     var y2 = 1985+i;
     var year = 'classification_'+y;
     var year2 = 'classification_'+y2;
     var sforest = sforest_all.select(year);
     var acm_forest = empty.select(year2).add(sforest);
-    var oldvalues = ee.List([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]);
-    var newvalues = ee.List([0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]);
+    var oldvalues = ee.List([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]);
+    var newvalues = ee.List([0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]);
     var remap = acm_forest.remap(oldvalues,newvalues);
     empty = empty.addBands(remap.multiply(mapbiomas_forest.select(year)).rename(ee.String(year)));
 }
@@ -79,15 +75,15 @@ var empty2 = ee.Image().byte();
 var ext = sforest_all.select('classification_1986');
 ext = ext.rename(ee.String('classification_1986'));
 empty = empty.addBands(ext);
-for (var i=1; i<33; i++)  {
+for (var i=1; i<35; i++)  {
     var y = 1986+i;
     var y2 = 1985+i;
     var year = 'classification_'+y;
     var year2 = 'classification_'+y2;
     var sforest = sforest_all.select(year);
     var acm_forest = empty.select(year2).add(sforest);
-    var oldvalues = ee.List([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]);
-    var newvalues = ee.List([0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]);
+    var oldvalues = ee.List([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]);
+    var newvalues = ee.List([0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]);
     var remap = acm_forest.remap(oldvalues,newvalues);
     var mask = mapbiomas_forest.select(year).remap([0,1],[500,1]);
     var loss = remap.add(mask).remap([1,2,500,501],[0,0,0,1]);
@@ -103,7 +99,7 @@ age = age.rename(ee.String('classification_1986'));
 empty = empty.addBands(age);
 empty = empty.slice(1);
 var temp = empty;
-for (var i=1; i<33; i++)  {
+for (var i=1; i<35; i++)  {
     var y = 1986+i;
     var year = 'classification_'+y;
     var sforest = sforest_ext.select(year);
@@ -118,7 +114,7 @@ var sforest_age = temp;
 // Export Products Data to Google Drive
 Export.image.toDrive({
           image: sforest_all,
-          description: 'secondary_forest_increment_collection41_v1', 
+          description: 'secondary_forest_increment_collection6_v4', 
           scale: 30, 
           region: brazil,
           maxPixels:1e13
@@ -126,7 +122,7 @@ Export.image.toDrive({
 
 Export.image.toDrive({
           image: sforest_ext,
-          description: 'secondary_forest_extent_collection41_v1', 
+          description: 'secondary_forest_extent_collection6_v4', 
           scale: 30, 
           region: brazil,
           maxPixels:1e13
@@ -134,7 +130,7 @@ Export.image.toDrive({
 
 Export.image.toDrive({
          image: sforest_age,
-          description: 'secondary_forest_age_collection41_v1', 
+          description: 'secondary_forest_age_collection6_v4', 
           scale: 30, 
           region: brazil,
           maxPixels:1e13
@@ -142,7 +138,7 @@ Export.image.toDrive({
 
 Export.image.toDrive({
          image: sforest_loss,
-          description: 'secondary_forest_loss_collection41_v1', 
+          description: 'secondary_forest_loss_collection6_v4', 
           scale: 30, 
           region: brazil,
           maxPixels:1e13
@@ -153,7 +149,7 @@ Export.image.toDrive({
 //Export Products Data to Asset
 Export.image.toAsset({
           image: sforest_all,
-          description: 'secondary_forest_increment_collection41_v1', 
+          description: 'secondary_forest_increment_collection6_v4', 
           scale: 30, 
           region: brazil,
           maxPixels:1e13
@@ -161,7 +157,7 @@ Export.image.toAsset({
 
 Export.image.toAsset({
           image: sforest_ext,
-          description: 'secondary_forest_extent_collection41_v1', 
+          description: 'secondary_forest_extent_collection6_v4', 
           scale: 30, 
           region: brazil,
           maxPixels:1e13
@@ -169,7 +165,7 @@ Export.image.toAsset({
 
 Export.image.toAsset({
           image: sforest_age,
-          description: 'secondary_forest_age_collection41_v2', 
+          description: 'secondary_forest_age_collection6_v4', 
           scale: 30, 
           region: brazil,
           maxPixels:1e13
@@ -177,7 +173,7 @@ Export.image.toAsset({
 
 Export.image.toAsset({
           image: sforest_loss,
-          description: 'secondary_forest_loss_collection41_v2', 
+          description: 'secondary_forest_loss_collection6_v4', 
           scale: 30, 
           region: brazil,
           maxPixels:1e13
