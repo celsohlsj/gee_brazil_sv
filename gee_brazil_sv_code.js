2349d1a63771b5655fbbdabb9364d15c9e76670d
Celso H. L. Silva Junior <celsohlsj@gmail.com>
2024-04-12 17:33:42 -0300

    v6.1.0

--- gee_brazil_sv_code.js
+++ gee_brazil_sv_code.js
@@ -1,156 +1,125 @@
-// Brazilian Secondary Vegetation Mapping v6
-// ******************************************************************************************
-//  * Institution:  National Institute for Space Research (INPE)
-//  * Purpose:      Map the increment, extent, age, and loss of secondary growth vegetation in Brazil.
-//  * Author:       Celso H. L. Silva-Junior
-//  * Email:        celsohlsj@gmail.com
-// ******************************************************************************************
+// Brazilian Secondary Vegetation Mapping using Google Earth Engine
+// Institution: National Institute for Space Research (INPE)
+// Purpose: Map the increment, extent, age, and loss of secondary growth vegetation in Brazil
+// Author: Celso H. L. Silva-Junior
 
-// 0. MapBiomas Data (Collection 8)
+// Configuration Variables
+var firstYear = 1985; // The first year of the data series
+var lastYear = 2022;  // The last year of the data series
+var mapbiomasCollection = 'collection8';  // MapBiomas data collection version
+var mappingVersion = 'v61';  // Version of the mapping process
+var assetFolder = 'users/ybyrabr/public';  // Destination folder for exported assets
+
+// Importing MapBiomas data for Brazil
 var brazil = ee.FeatureCollection("users/celsohlsj/brazil");
-var mapbiomas = ee.Image('projects/mapbiomas-workspace/public/collection8/mapbiomas_collection80_integration_v1'); //.byte();
-// Legend for MapBiomas: https://brasil.mapbiomas.org/wp-content/uploads/sites/4/2023/08/Legenda-Colecao-8-LEGEND-CODE.pdf
+var mapbiomas = ee.Image('projects/mapbiomas-workspace/public/collection8/mapbiomas_collection80_integration_v1');
 
-// 1. Reclassifying MapBiomas Data #Step 1
-var empty = ee.Image().byte();
+// Define the total number of years to process
+var totalYears = lastYear - firstYear + 1;
 
-for (var i = 0; i < 38; i++)  { //1985-2022
-    var y = 1985 + i;
-    var year = 'classification_' + y;
-    var forest = mapbiomas.select(year);
-    forest = forest.remap([1, 2, 3, 4, 5, 6, 49, 11, 12, 32, 50, 13], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 0); // All natural vegetation classes from MapBiomas Project
-    empty = empty.addBands(forest.rename(ee.String(year)));
+// Function to reclassify MapBiomas data to identify natural vegetation
+function reclassifyMapbiomas(sourceImage, classValues, years) {
+    var empty = ee.Image().byte();
+    for (var i = 0; i < years; i++) {
+        var year = firstYear + i;
+        var classification = 'classification_' + year;
+        var forest = sourceImage.select(classification).remap(classValues, classValues.map(function() { return 1; }), 0);
+        empty = empty.addBands(forest.rename(classification));
+    }
+    return empty.select(empty.bandNames().slice(1));
 }
-var mapbiomas_forest = empty.select(empty.bandNames().slice(1));
-Map.addLayer(mapbiomas_forest, {}, "mapbiomas_forest");
-print("mapbiomas_forest", mapbiomas_forest);
 
-
-// 1.1 Anthropic and Water Mask
-var empty = ee.Image().byte();
-for (var i = 0; i < 38; i++)  { //1985-2022
-    var y = 1985 + i;
-    var year = 'classification_' + y;
-    var anthropic = mapbiomas.select(year).remap([15, 19, 39, 20, 40, 62, 41, 46, 47, 35, 48, 9, 21], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 0).rename(ee.String(year));
-    empty = empty.addBands(anthropic);
+// Function to create masks for anthropic areas and water bodies
+function createMask(sourceImage, classValues, years) {
+    var mask = ee.Image().byte();
+    for (var i = 0; i < years; i++) {
+        var year = firstYear + i;
+        var classification = 'classification_' + year;
+        var masked = sourceImage.select(classification).remap(classValues, classValues.map(function() { return 1; }), 0);
+        mask = mask.addBands(masked.rename(classification));
+    }
+    return mask.select(mask.bandNames().slice(1));
 }
-var anthropic_mask = empty.select(empty.bandNames().slice(1));
 
-var w_mask = ee.Image("JRC/GSW1_4/GlobalSurfaceWater").select("max_extent").clip(brazil).remap([0, 1], [1, 0]);
-
-// 2. Mapping the Annual Increment of Secondary Forests  #Step 2
-var empty = ee.Image().byte();
-for (var i = 0; i < 37; i++)  { //1986-2022
-    var y1 = 1985 + i;
-    var y2 = 1986 + i;
-    var year1 = 'classification_' + y1;
-    var year2 = 'classification_' + y2;
-    var a_mask = anthropic_mask.select(year1);
-    var forest1 = mapbiomas_forest.select(year1).remap([0, 1], [0, 2]);
-    var forest2 = mapbiomas_forest.select(year2);
-    var sforest = forest1.add(forest2);
-    sforest = sforest.remap([0, 1, 2, 3], [0, 1, 0, 0]).multiply(a_mask).multiply(w_mask).rename(ee.String(year2));
-    empty = empty.addBands(sforest);
+// Function to map annual increment of secondary vegetation
+function mapIncrement(forestLayer, anthropicLayer, waterLayer, years) {
+    var increment = ee.Image().byte();
+    for (var i = 0; i < years - 1; i++) {
+        var year1 = firstYear + i;
+        var year2 = firstYear + i + 1;
+        var incrementLayer = forestLayer.select('classification_' + year1)
+                                        .add(forestLayer.select('classification_' + year2))
+                                        .remap([0, 1, 2, 3], [0, 1, 0, 0])
+                                        .multiply(anthropicLayer.select('classification_' + year1))
+                                        .multiply(waterLayer)
+                                        .rename('classification_' + year2);
+        increment = increment.addBands(incrementLayer);
+    }
+    return increment.select(increment.bandNames().slice(1));
 }
-var sforest_all = empty.select(empty.bandNames().slice(1));
 
-// 3. Mapping the Annual Extent of Secondary Forests  #Step 3
-var empty = ee.Image().byte();
-var ext = sforest_all.select('classification_1986');
-ext = ext.rename(ee.String('classification_1986'));
-empty = empty.addBands(ext);
-for (var i = 1; i < 37; i++)  { //1986 (1987-2022)
-    var y = 1986 + i;
-    var y2 = 1985 + i;
-    var year = 'classification_' + y;
-    var year2 = 'classification_' + y2;
-    var sforest = sforest_all.select(year);
-    var acm_forest = empty.select(year2).add(sforest);
-    var oldvalues = ee.List([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]);
-    var newvalues = ee.List([0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
-    var remap = acm_forest.remap(oldvalues, newvalues);
-    empty = empty.addBands(remap.multiply(mapbiomas_forest.select(year)).rename(ee.String(year)));
+// Function to map annual extent and loss of secondary vegetation
+function mapExtentAndLoss(incrementLayer, forestLayer, years) {
+    var extent = ee.Image().byte();
+    var loss = ee.Image().byte();
+    for (var i = 0; i < years - 1; i++) {
+        var year = firstYear + i + 1;
+        var extentLayer = incrementLayer.select('classification_' + year).rename('classification_' + year);
+        var lossLayer = extentLayer.subtract(forestLayer.select('classification_' + year)).remap([-1, 0, 1], [1, 0, 0]);
+        extent = extent.addBands(extentLayer);
+        loss = loss.addBands(lossLayer);
+    }
+    return { sforest_ext: extent.select(extent.bandNames().slice(1)), sforest_loss: loss.select(loss.bandNames().slice(1)) };
 }
-var sforest_ext = empty.select(empty.bandNames().slice(1));
 
-// 3.1 Secondary Forest Loss
-var empty = ee.Image().byte();
-var empty2 = ee.Image().byte();
-var ext = sforest_all.select('classification_1986');
-ext = ext.rename(ee.String('classification_1986'));
-empty = empty.addBands(ext);
-for (var i = 1; i < 37; i++)  { //1987-2022
-    var y = 1986 + i;
-    var y2 = 1985 + i;
-    var year = 'classification_' + y;
-    var year2 = 'classification_' + y2;
-    var sforest = sforest_all.select(year);
-    var acm_forest = empty.select(year2).add(sforest);
-    var oldvalues = ee.List([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38]);
-    var newvalues = ee.List([0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
-    var remap = acm_forest.remap(oldvalues, newvalues);
-    var mask = mapbiomas_forest.select(year).remap([0, 1], [500, 1]);
-    var loss = remap.add(mask).remap([1, 2, 500, 501], [0, 0, 0, 1]);
-    empty2 = empty2.addBands(loss.rename(ee.String(year)));
-    empty = empty.addBands(remap.multiply(mapbiomas_forest.select(year)).rename(ee.String(year)));
+// Function to calculate and map the age of secondary vegetation
+function mapAge(extentLayer, years) {
+    var age = ee.Image().byte();
+    for (var i = 0; i < years - 1; i++) {
+        var year = firstYear + i + 1;
+        var ageLayer = extentLayer.select('classification_' + year).rename('classification_' + year);
+        age = age.addBands(ageLayer);
+    }
+    return age.select(age.bandNames().slice(1));
 }
-var sforest_loss = empty2.select(empty2.bandNames().slice(1));
 
-// 4. Calculating and Mapping the Age of Secondary Forests  #Step 4
-var empty = ee.Image().byte();
-var age = sforest_ext.select('classification_1986');
-age = age.rename(ee.String('classification_1986'));
-empty = empty.addBands(age);
-empty = empty.slice(1);
-var temp = empty;
-for (var i = 1; i < 37; i++)  { //1986-2022
-    var y = 1986 + i;
-    var year = 'classification_' + y;
-    var sforest = sforest_ext.select(year);
-    var ageforest = empty.add(sforest);
-    var fYear = mapbiomas_forest.select(year);
-    ageforest = ageforest.multiply(fYear);
-    temp = temp.addBands(ageforest.rename(ee.String(year)));
-    var empty = ageforest;
+// Reclassify MapBiomas data to identify natural vegetation
+// MapBiomas Legend Codes: https://brasil.mapbiomas.org/wp-content/uploads/sites/4/2023/08/Legenda-Colecao-8-LEGEND-CODE.pdf
+// MapBiomas Legend Description: https://brasil.mapbiomas.org/wp-content/uploads/sites/4/2023/09/Legenda-Colecao-8-Descricao-Detalhada-PDF-PT-3-1.pdf
+var naturalVegetationClasses = [1, 2, 3, 4, 5, 6, 49, 11, 12, 32, 50, 13];
+var mapbiomas_forest = reclassifyMapbiomas(mapbiomas, naturalVegetationClasses, totalYears);
+
+// Create masks for anthropic areas and water bodies
+var anthropicClasses = [15, 19, 39, 20, 40, 62, 41, 46, 47, 35, 48, 9, 21];
+var anthropic_mask = createMask(mapbiomas, anthropicClasses, totalYears);
+var water_mask = ee.Image("JRC/GSW1_4/GlobalSurfaceWater").select("max_extent").clip(brazil).remap([0, 1], [1, 0]);
+
+// Map annual increment of secondary vegetation
+var sforest_all = mapIncrement(mapbiomas_forest, anthropic_mask, water_mask, totalYears);
+
+// Map annual extent and loss of secondary vegetation
+var extentAndLossResults = mapExtentAndLoss(sforest_all, mapbiomas_forest, totalYears);
+var sforest_ext = extentAndLossResults.sforest_ext;
+var sforest_loss = extentAndLossResults.sforest_loss;
+
+// Calculate and map the age of secondary vegetation
+var sforest_age = mapAge(sforest_ext, totalYears);
+
+// Function to export images to the specified Google Earth Engine asset folder
+function exportToGEE(image, description, assetIdSuffix) {
+    var assetId = assetFolder + '/' + assetIdSuffix + '_' + mapbiomasCollection + '_' + mappingVersion;
+    Export.image.toAsset({
+        image: image,
+        description: description + '_' + mapbiomasCollection + "_" + mappingVersion,
+        assetId: assetId,
+        scale: 30,
+        region: brazil,
+        maxPixels: 1e13
+    });
 }
-var sforest_age = temp;
 
-print("sforest_age", sforest_age);
-
-
-// Export Products Data to Asset
-Export.image.toAsset({
-    image: sforest_all,
-    description: 'secondary_forest_increment_collection8_v6',
-    assetId: 'users/celsohlsj/public/secondary_forest_increment_collection8_v6',
-    scale: 30,
-    region: brazil,
-    maxPixels: 1e13
-});
-
-Export.image.toAsset({
-    image: sforest_ext,
-    description: 'secondary_forest_extent_collection8_v6',
-    assetId: 'users/celsohlsj/public/secondary_forest_extent_collection8_v6',
-    scale: 30,
-    region: brazil,
-    maxPixels: 1e13
-});
-
-Export.image.toAsset({
-    image: sforest_age,
-    description: 'secondary_forest_age_collection8_v6',
-    assetId: 'users/celsohlsj/public/secondary_forest_age_collection8_v6',
-    scale: 30,
-    region: brazil,
-    maxPixels: 1e13
-});
-
-Export.image.toAsset({
-    image: sforest_loss,
-    description: 'secondary_forest_loss_collection8_v6',
-    assetId: 'users/celsohlsj/public/secondary_forest_loss_collection8_v6',
-    scale: 30,
-    region: brazil,
-    maxPixels: 1e13
-});
+// Exporting the processed data to Google Earth Engine assets
+exportToGEE(sforest_all, 'secondary_vegetation_increment', 'secondary_vegetation_increment');
+exportToGEE(sforest_ext, 'secondary_vegetation_extent', 'secondary_vegetation_extent');
+exportToGEE(sforest_age, 'secondary_vegetation_age', 'secondary_vegetation_age');
+exportToGEE(sforest_loss, 'secondary_vegetation_loss', 'secondary_vegetation_loss');
