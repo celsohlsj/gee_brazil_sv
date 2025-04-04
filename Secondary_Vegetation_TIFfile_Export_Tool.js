f288468f4ade02dd1f7ed6ce49f866d8d74a5a6a
Celso H. L. Silva Junior <celsohlsj@gmail.com>
2025-04-04 15:46:06 -0300

    Update Secondary_Vegetation_TIFfile_Export_Tool.js

--- Secondary_Vegetation_TIFfile_Export_Tool.js
+++ Secondary_Vegetation_TIFfile_Export_Tool.js
@@ -1,21 +1,22 @@
 ///// Assets /////
-var loss = ee.Image("users/ybyrabr/public/secondary_vegetation_loss_collection8_v61"),
-    age = ee.Image("users/ybyrabr/public/secondary_vegetation_age_collection8_v61"),
-    extent = ee.Image("users/ybyrabr/public/secondary_vegetation_extent_collection8_v61"),
-    increment = ee.Image("users/ybyrabr/public/secondary_vegetation_increment_collection8_v61"),
-    mapbiomas = ee.Image('projects/mapbiomas-workspace/public/collection8/mapbiomas_collection80_integration_v1');
+var loss = ee.Image("users/ybyrabr/public/secondary_vegetation_loss_collection9_v71"),
+    age = ee.Image("users/ybyrabr/public/secondary_vegetation_age_collection9_v71"),
+    extent = ee.Image("users/ybyrabr/public/secondary_vegetation_extent_collection9_v71"),
+    increment = ee.Image("users/ybyrabr/public/secondary_vegetation_increment_collection9_v71"),
+    mapbiomas = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection9/mapbiomas_collection90_integration_v1');
 
 // Area of Interest (Brazil)
+// Here the user can replace it with their area of ​​interest
 var aoi = ee.FeatureCollection("users/celsohlsj/brazil");
 
 ///// Type of Vegetation /////
 // Here you select the type of secondary vegetation to be exported (forest or non-forest).
-// Please refer to the legend document: https://brasil.mapbiomas.org/wp-content/uploads/sites/4/2023/08/Legenda-Colecao-8-LEGEND-CODE.pdf
+// Please refer to the legend document: https://brasil.mapbiomas.org/wp-content/uploads/sites/4/2024/10/Legenda-Colecao-9-LEGEND-CODE_v2.pdf
 var type = 3; // 1.1. Forest Formation: 3
 var vegetation_type = mapbiomas.select('classification_1985').eq(type);
 
 // Save Datasets
-for (var i=1986; i<2023; i++)  {
+for (var i=1986; i<2024; i++)  {
     var y = i;
     var year = 'classification_'+y;
     var export_year = age.select(year).multiply(vegetation_type);
@@ -29,7 +30,7 @@ for (var i=1986; i<2023; i++)  {
 });
 }
 
-for (var i=1986; i<2023; i++)  {
+for (var i=1986; i<2024; i++)  {
     var y = i;
     var year = 'classification_'+y;
     var export_year = extent.select(year).multiply(vegetation_type);
@@ -43,7 +44,7 @@ for (var i=1986; i<2023; i++)  {
 });
 }
 
-for (var i=1986; i<2023; i++)  {
+for (var i=1986; i<2024; i++)  {
     var y = i;
     var year = 'classification_'+y;
     var export_year = increment.select(year).multiply(vegetation_type);
@@ -57,7 +58,7 @@ for (var i=1986; i<2023; i++)  {
 });
 }
 
-for (var i=1987; i<2022; i++)  {
+for (var i=1987; i<2024; i++)  {
     var y = i;
     var year = 'classification_'+y;
     var export_year = loss.select(year).multiply(vegetation_type);
