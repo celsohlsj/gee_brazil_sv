5a54d3c5c1ca6cbac79a28f370370ec051906a20
Celso H. L. Silva Junior <celsohlsj@gmail.com>
2025-04-04 16:05:49 -0300

    Update Secondary_Vegetation_TIFfile_Export_Tool.js

--- Secondary_Vegetation_TIFfile_Export_Tool.js
+++ Secondary_Vegetation_TIFfile_Export_Tool.js
@@ -1,73 +1,77 @@
+// Configuration variables
+var collection = 'collection9';         // Collection name
+var mappingVersion = 'v71';               // Mapping version
+var lastYear = 2023;                      // Last year to export
+var startYear = 1986;                     // Start year for age, extent, and increment datasets
+var lossStartYear = 1987;                 // Start year for loss dataset
+
 ///// Assets /////
-var loss = ee.Image("users/ybyrabr/public/secondary_vegetation_loss_collection9_v71"),
-    age = ee.Image("users/ybyrabr/public/secondary_vegetation_age_collection9_v71"),
-    extent = ee.Image("users/ybyrabr/public/secondary_vegetation_extent_collection9_v71"),
-    increment = ee.Image("users/ybyrabr/public/secondary_vegetation_increment_collection9_v71"),
+var loss = ee.Image("users/ybyrabr/public/secondary_vegetation_loss_" + collection + "_" + mappingVersion),
+    age = ee.Image("users/ybyrabr/public/secondary_vegetation_age_" + collection + "_" + mappingVersion),
+    extent = ee.Image("users/ybyrabr/public/secondary_vegetation_extent_" + collection + "_" + mappingVersion),
+    increment = ee.Image("users/ybyrabr/public/secondary_vegetation_increment_" + collection + "_" + mappingVersion),
     mapbiomas = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection9/mapbiomas_collection90_integration_v1');
 
 // Area of Interest (Brazil)
-// Here the user can replace it with their area of ​​interest
+// Replace this with your own area of interest if needed.
 var aoi = ee.FeatureCollection("users/celsohlsj/brazil");
 
-///// Type of Vegetation /////
-// Here you select the type of secondary vegetation to be exported (forest or non-forest).
-// Please refer to the legend document: https://brasil.mapbiomas.org/wp-content/uploads/sites/4/2024/10/Legenda-Colecao-9-LEGEND-CODE_v2.pdf
+///// Vegetation Type /////
+// Select the type of secondary vegetation to export (forest or non-forest).
+// Refer to the legend: https://brasil.mapbiomas.org/wp-content/uploads/sites/4/2024/10/Legenda-Colecao-9-LEGEND-CODE_v2.pdf
 var type = 3; // 1.1. Forest Formation: 3
 var vegetation_type = mapbiomas.select('classification_1985').eq(type);
 
-// Save Datasets
-for (var i=1986; i<2024; i++)  {
-    var y = i;
-    var year = 'classification_'+y;
-    var export_year = age.select(year).multiply(vegetation_type);
+// Export datasets for "age", "extent", and "increment"
+for (var i = startYear; i < lastYear + 1; i++)  {
+    var year = 'classification_' + i;
+    var exportImage = age.select(year).multiply(vegetation_type);
     Export.image.toDrive({
-          image: export_year,
-          description: 'secondary_vegetation_age_v61_'+y, 
+          image: exportImage,
+          description: 'secondary_vegetation_age_' + mappingVersion + '_' + i, 
           scale: 30, 
           region: aoi,
-          folder: 'secondary_vegetation_collection8_v61',
-          maxPixels:1e13
-});
+          folder: 'secondary_vegetation_' + collection + '_' + mappingVersion,
+          maxPixels: 1e13
+    });
 }
 
-for (var i=1986; i<2024; i++)  {
-    var y = i;
-    var year = 'classification_'+y;
-    var export_year = extent.select(year).multiply(vegetation_type);
+for (var i = startYear; i < lastYear + 1; i++)  {
+    var year = 'classification_' + i;
+    var exportImage = extent.select(year).multiply(vegetation_type);
     Export.image.toDrive({
-          image: export_year,
-          description: 'secondary_vegetation_extent_v61_'+y, 
+          image: exportImage,
+          description: 'secondary_vegetation_extent_' + mappingVersion + '_' + i, 
           scale: 30, 
           region: aoi,
-          folder: 'secondary_vegetation_collection8_v61',
-          maxPixels:1e13
-});
+          folder: 'secondary_vegetation_' + collection + '_' + mappingVersion,
+          maxPixels: 1e13
+    });
 }
 
-for (var i=1986; i<2024; i++)  {
-    var y = i;
-    var year = 'classification_'+y;
-    var export_year = increment.select(year).multiply(vegetation_type);
+for (var i = startYear; i < lastYear + 1; i++)  {
+    var year = 'classification_' + i;
+    var exportImage = increment.select(year).multiply(vegetation_type);
     Export.image.toDrive({
-          image: export_year,
-          description: 'secondary_vegetation_increment_v61_'+y, 
+          image: exportImage,
+          description: 'secondary_vegetation_increment_' + mappingVersion + '_' + i, 
           scale: 30, 
           region: aoi,
-          folder: 'secondary_vegetation_collection8_v61',
-          maxPixels:1e13
-});
+          folder: 'secondary_vegetation_' + collection + '_' + mappingVersion,
+          maxPixels: 1e13
+    });
 }
 
-for (var i=1987; i<2024; i++)  {
-    var y = i;
-    var year = 'classification_'+y;
-    var export_year = loss.select(year).multiply(vegetation_type);
+// Export datasets for "loss" (starting in 1987)
+for (var i = lossStartYear; i < lastYear + 1; i++)  {
+    var year = 'classification_' + i;
+    var exportImage = loss.select(year).multiply(vegetation_type);
     Export.image.toDrive({
-          image: export_year,
-          description: 'secondary_vegetation_loss_v61_'+y, 
+          image: exportImage,
+          description: 'secondary_vegetation_loss_' + mappingVersion + '_' + i, 
           scale: 30, 
           region: aoi,
-          folder: 'secondary_vegetation_collection8_v5',
-          maxPixels:1e13
-});
+          folder: 'secondary_vegetation_' + collection + '_' + mappingVersion,
+          maxPixels: 1e13
+    });
 }
